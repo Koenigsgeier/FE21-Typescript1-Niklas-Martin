301c2922b0c4a298272a32235bb09ad334018670
Martin Maier <martin.m@gmx.at>
2021-07-07 10:56:12 +0200

    basic one finished

---
+++ martin/script.js
@@ -0,0 +1,5 @@
+for (var i = 1; i <= 10; i++) {
+    for (var e = 1; e <= 10; e++) {
+        document.write(e + 'X' + i + '=' + e * i + '<br>');
+    }
+}
---
+++ martin/script.ts
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 10; i++) {    
+
+  for (let e = 1; e <= 10; e++) {
+      document.write(e + 'X' + i + '=' + e*i + '<br>')
+      
+  }
+
+}
